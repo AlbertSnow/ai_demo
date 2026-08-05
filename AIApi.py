33e7fc7f73f22f26eeb1534c411aa8f732ed549f
AlbertSnow <albert.snow.email@gmail.com>
2026-08-05 17:40:08 +0800

    first vary message inlien

--- AIApi.py
+++ AIApi.py
@@ -1,6 +1,7 @@
 import os
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage
 
 load_dotenv()
 
@@ -11,10 +12,16 @@ llm = ChatOpenAI(
 )
 
 def get_ai_response(prompt):
-    response = llm.invoke(prompt)
+    # response = llm.invoke(prompt)
+    response = llm.invoke(
+        [
+            SystemMessage(content="You are a helpful assistant."),
+            HumanMessage(content=prompt)
+        ]
+    )
     print(response.content)
     return response.content
 
 
 if __name__ == "__main__":
-    get_ai_response("What is the capital of France?")
+    get_ai_response("What is the capital of China?")
